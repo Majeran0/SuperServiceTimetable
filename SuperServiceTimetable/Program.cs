8ac54b9aa1f2a457c746b64fcc1f39ac216827ab
Filip Fidkowski <tropok@gmail.com>
2022-03-18 21:15:28 +0100

    docker thingies

--- SuperServiceTimetable/Program.cs
+++ SuperServiceTimetable/Program.cs
@@ -12,8 +12,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+else
+{
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
